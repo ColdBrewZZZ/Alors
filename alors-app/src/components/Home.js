4879e3abe31b2b5b7363ba774b1270b03b064a5e
zoe <z.g.bradford@gmail.com>
2023-06-21 06:29:09 -0500

    nav css

--- alors-app/src/components/Home.js
+++ alors-app/src/components/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Home.css';
+import { Button } from './Button';
+import './Navbar.css';
 function Home() {
-  
+    const [button, setButton] = useState(true)
 
   return (
     <div>
       <h2>Home</h2>
-      
+      {button && <Button buttonStyle='btn--outline'>More</Button>}
     </div>
   );
 }
