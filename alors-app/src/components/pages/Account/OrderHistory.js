7a9474f850358d7728cbfca593d349efc0aeba21
zoe <z.g.bradford@gmail.com>
2023-08-27 13:30:15 -0500

    hello Bunny

--- alors-app/src/components/pages/Account/OrderHistory.js
+++ alors-app/src/components/pages/Account/OrderHistory.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import image from '../../../img/exploreAp.JPG';
 import './OrderHistory.css';
 import { Button } from 'react-bootstrap';
+import axios from '../../../api/axios';
 
 function OrderHistory() {
-  
+    const [user, setUser] = useState({ first_name: "user" });
+
+    useEffect(() => {
+        // Fetch user data from your API
+        axios.get('http://localhost:3000/users/user', {
+            withCredentials: true,
+        })
+        .then(response => {
+            // Update the user state with the data from the API
+            setUser(response.data[0]); // Assuming the API returns an array with a single user object
+        })
+        .catch(error => {
+            console.error('Error setting user:', error);
+        });
+    }, []); // Empty dependency array to run the effect only once
+
+    
 
   return (
     <>
         <div>
+            <h1>Hello {user.first_name} </h1>
             <h1 className="text-center">ORDER HISTORY</h1>
         </div>
         <div className="container d-flex">
