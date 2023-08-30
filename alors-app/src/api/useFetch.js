409d5d7df307cc0aa17caa06877f6b2156fef11a
zoe <z.g.bradford@gmail.com>
2023-08-30 15:13:17 -0500

    custom hooks

---
+++ alors-app/src/api/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+function useFetch(url, options = {}) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+
+    fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, [url, options]);
+
+  return { data, isLoading, error };
+}
+
+export default useFetch;
