e1636d19eab56751db7424c169e71e82a12a3f37
zoe <z.g.bradford@gmail.com>
2023-08-31 11:45:12 -0500

    reset user password from reset page

--- alors-app/src/components/pages/PasswordReset.js
+++ alors-app/src/components/pages/PasswordReset.js
@@ -9,33 +9,85 @@ import axios from '../../api/axios';
 function PasswordReset() {
   
 
- 
+    const formFields = [
+     
+        { label: 'Password', name: 'password', type:'password'},
+        { label: 'Email', name: 'email', type:'email'}
+      ];
+
   
+  const [info, setInfo] = useState({
+    
+    password: '',
+    email: '',
+    
+  });
+
+  
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInfo({
+      ...info,
+      [name]: value,
+    });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      
+      const response = await fetch('http://localhost:3000/reset_password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(info),
+      });
+
+      if (response.ok) {
+        console.log("hello")
+      } else {
+        console.log("no no", response)
+      }
+    } catch (error) {
+      console.error('Error changing password for the user:', error);
+    }
+  };
+
   return (
     <>
-      <div className="text-center mt-4">
-        <h1>Make Your New Password</h1>
+    <div className="text-center mt-4">
+        <h1>RESET YOUR PASSWORD</h1>
         <hr />
+    </div>
+    <div className="container d-flex justify-content-center align-items-center" >
+      <div className="text-center col-md-6 bg-white rounded pt-5 p-4 my-4 border border-black" >
+        <div>  
+            {formFields.map((field, index) => (
+                    <div className="mb-3" key={index}>
+                    <Form.Group controlId={`form${field.name}`}>
+                        <InputGroup>
+                        <InputGroup.Text>{field.label}:</InputGroup.Text>
+                        <Form.Control
+                            type={field.type}
+                            name={field.name}
+                            value={info[field.name]}
+                            onChange={handleInputChange}
+                        />
+                        </InputGroup>
+                    </Form.Group>
+                    </div>
+                ))}
+        </div>
+        <Button onClick={handleSubmit}>SUBMIT</Button>
       </div>
-      <div className="enter-email-container">
-       
-            <label>New Password</label>
-            <input type="text" name="password" />
-            
-            <label>Reenter New Password:</label>
-            <input type="text" name="retypePassword"/>
+    </div>
+ 
       
-
-        <div className="mt-4 mb-5">
-           <Button>Save Changes</Button>
-        </div>
-        <div className='pb-5'>
-          <div className="mb-2">Don't have an account yet?</div>
-          <Link to="/Registration">make a new account</Link>
-        </div>
-       
-      </div>
-    </>
+      
+  </>
   );
 }
 
